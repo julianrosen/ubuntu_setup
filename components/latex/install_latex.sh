d1fd129d6956b0eeec1f4dc1c705021469bd6490
Julian Rosen <julianrosen@gmail.com>
2026-01-19 17:26:08 -0500

    Use local latex repo

--- components/latex/install_latex.sh
+++ components/latex/install_latex.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
 sudo apt install texlive-latex-extra texworks
-git clone git@github.com:julianrosen/latex.git $HOME/texmf
-ln -s $HOME/texmf/templates $HOME/.local/share/TUG/TeXworks/templates
+LATEX_REPO_DIR="$(dirname -- "$0")/../../../latex"
+
+if [[ ! -d "$LATEX_REPO_DIR" ]]; then
+  echo "Please clone the latex repo next to this repo (expected at: $LATEX_REPO_DIR)."
+  exit 1
+fi
+
+mkdir -p "$HOME/texmf"
+
+ln -s "$LATEX_REPO_DIR/bibtex" "$HOME/texmf/bibtex"
+ln -s "$LATEX_REPO_DIR/tex" "$HOME/texmf/tex"
+
+ln -s "$LATEX_REPO_DIR/templates" "$HOME/.local/share/TUG/TeXworks/templates"
