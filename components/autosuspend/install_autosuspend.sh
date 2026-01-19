db3a29e2ce321376d4de7c28d9cdd5b07d4303a7
Julian Rosen <julianrosen@gmail.com>
2026-01-19 17:18:00 -0500

    Simplify python and autosuspend installers

--- components/autosuspend/install_autosuspend.sh
+++ components/autosuspend/install_autosuspend.sh
@@ -3,12 +3,8 @@
 SCRIPT_DIR="$(dirname -- "$0")"
 
 sudo apt install autosuspend playerctl caffeine
-sed "s/@USER@/$USER/g" $SCRIPT_DIR/autosuspend.conf.in > $SCRIPT_DIR/autosuspend.conf
-sudo install $SCRIPT_DIR/autosuspend.conf  /etc/ -g root -o root -m 644
-rm $SCRIPT_DIR/autosuspend.conf
+sed "s/@USER@/$USER/g" "$SCRIPT_DIR/autosuspend.conf.in" | sudo tee /etc/autosuspend.conf >/dev/null
 
 
-sed "s/@USER@/$USER/g" $SCRIPT_DIR/update_playing_crontab.in > $SCRIPT_DIR/update_playing_crontab
-crontab $SCRIPT_DIR/update_playing_crontab
-rm $SCRIPT_DIR/update_playing_crontab
+sed "s/@USER@/$USER/g" "$SCRIPT_DIR/update_playing_crontab.in" | crontab
 mkdir $HOME/.playing
--- components/python/install_python.sh
+++ components/python/install_python.sh
@@ -3,9 +3,6 @@
 VENV_DIR="${1:-$HOME/.venvs}"
 ENV_NAME="${2:-general}"
 PIP_PACKAGES=("${@:3}")
-if [[ ${#PIP_PACKAGES[@]} -eq 0 ]]; then
-  PIP_PACKAGES=(scikit-learn statsmodels scikit-optimize numpy scipy pandas matplotlib seaborn xgboost torch bayesian_optimization ipykernel jupyter tqdm black isort)
-fi
 
 mkdir -p "$VENV_DIR"
 sudo apt install python3-pip python3-venv
