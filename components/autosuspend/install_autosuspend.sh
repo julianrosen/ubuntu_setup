0921d1e27f6da90de037232c191d182daf6dd92e
Julian Rosen <julianrosen@gmail.com>
2026-01-21 15:01:42 -0500

    Restart autosuspend after config update

--- components/autosuspend/install_autosuspend.sh
+++ components/autosuspend/install_autosuspend.sh
@@ -5,6 +5,9 @@ SCRIPT_DIR="$(dirname -- "$0")"
 sudo apt install autosuspend playerctl caffeine
 sed "s/@USER@/$USER/g" "$SCRIPT_DIR/autosuspend.conf.in" | sudo tee /etc/autosuspend.conf >/dev/null
 
+# Reload autosuspend to pick up config changes.
+sudo systemctl restart autosuspend
+
 
 # Add the update_playing cron entry if it is not already present.
 CRON_LINE="*/2 * * * * $HOME/bin/update_playing"
