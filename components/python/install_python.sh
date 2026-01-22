0201472b60972d07bcb5ba664e4130036e912e15
Julian Rosen <julianrosen@gmail.com>
2026-01-21 19:43:25 -0500

    Make venv location home-relative

--- components/python/install_python.sh
+++ components/python/install_python.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-VENV_DIR="${1:-$HOME/.venvs}"
+VENV_DIR_REL="${1:-.venvs}"
 ENV_NAME="${2:-general}"
 PIP_PACKAGES=("${@:3}")
 
+VENV_DIR="$HOME/$VENV_DIR_REL"
 mkdir -p "$VENV_DIR"
 sudo apt install python3-pip python3-venv
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
