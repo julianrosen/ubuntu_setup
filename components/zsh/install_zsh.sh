599afbe2f7feb42d8a1f9f943d7641a6aa62d525
Julian Rosen <julianrosen@gmail.com>
2026-01-21 19:50:30 -0500

    Prompt before overwriting zshrc

--- components/zsh/install_zsh.sh
+++ components/zsh/install_zsh.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 sudo apt install zsh
+if [[ -e "$HOME/.zshrc" ]]; then
+  read -r -p "Overwrite existing $HOME/.zshrc? [y/N] " RESPONSE
+  if [[ "$RESPONSE" != "y" && "$RESPONSE" != "Y" ]]; then
+    exit 0
+  fi
+fi
 install "$(dirname -- "$0")/zshrc" "$HOME/.zshrc"
