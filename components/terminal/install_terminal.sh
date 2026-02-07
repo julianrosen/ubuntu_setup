d53d1766f7653a9104d063b3d4adebe30777a0be
Julian Rosen <julianrosen@gmail.com>
2026-02-06 22:50:58 -0500

    Hide menu bar

--- components/terminal/install_terminal.sh
+++ components/terminal/install_terminal.sh
@@ -4,3 +4,5 @@ SCRIPT_DIR="$(dirname -- "$0")"
 
 dconf load /org/gnome/terminal/legacy/profiles:/ < $SCRIPT_DIR/gnome_terminal_profile.dconf
 dconf load /org/gnome/terminal/legacy/keybindings/ < $SCRIPT_DIR/gnome_terminal_keybindings.dconf
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+
